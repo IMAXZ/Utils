7ffc8a55e4b4f289db28a94764fac4c7fee7728e
郑鑫 <822262059@qq.com>
2020-05-29 16:51:52 +0800

    添加log4net测试

---
+++ Test/Log4NetTest.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Log4NetTest
+    {
+        static void Main(string[] args)
+        {
+            ILog log = LogManager.GetLogger(typeof(Log4NetTest));
+            log.Info("info");
+            log.Error("Error");
+        }
+    }
+}
